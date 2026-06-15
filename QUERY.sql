5047afedc5a0b2addbff8244424b91588312fba9
arafathussendev <arafathussen00@gmail.com>
2026-06-15 18:25:29 +0600

    add left join

--- QUERY.sql
+++ QUERY.sql
@@ -157,3 +157,13 @@ SELECT
 FROM Bookings b
 INNER JOIN Users u   ON b.user_id  = u.user_id
 INNER JOIN Matches m ON b.match_id = m.match_id;
+
+
+-- Query 5: All Users with Bookings (LEFT JOIN)
+
+SELECT
+    u.user_id,
+    u.full_name,
+    b.booking_id
+FROM Users u
+LEFT JOIN Bookings b ON u.user_id = b.user_id;
