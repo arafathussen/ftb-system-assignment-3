3e9c50d51c36c47dd8a65d1972beb6dad8c2d331
arafathussendev <arafathussen00@gmail.com>
2026-06-15 18:22:05 +0600

    add null payment status finder

--- QUERY.sql
+++ QUERY.sql
@@ -134,3 +134,14 @@ SELECT user_id, full_name, email
 FROM Users
 WHERE full_name ILIKE 'Tanvir%'
    OR full_name ILIKE '%Haque%';
+
+
+   -- Query 3: NULL Payment Status Finder
+
+SELECT
+    booking_id,
+    user_id,
+    match_id,
+    COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM Bookings
+WHERE payment_status IS NULL;
